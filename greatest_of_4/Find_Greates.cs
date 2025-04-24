371e779f37fe5b69a591c254fa76eaaea5d93b68
Sakthi-kc <kcsakthi17@gmail.com>
2025-04-24 22:21:13 +0530

    Edited the calc sln and added a new project

---
+++ greatest_of_4/Find_Greates.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace greatest_of_4
+{
+    class Find_Greatest
+    {
+        public decimal FirstNumber { get; set; }
+
+        public decimal SecondNumber { get; set; }
+
+        public decimal ThirdNumber { get; set; }
+
+        public decimal FourthNumber { get; set; }
+
+        decimal GreatestNumber = 0;
+
+        public decimal GreatestOf4()
+        {
+            Console.WriteLine($"The four numbers are {FirstNumber}, {SecondNumber}, {ThirdNumber} and {FourthNumber}");
+
+            if (FirstNumber >= SecondNumber && FirstNumber >= ThirdNumber)
+            {
+                if(FirstNumber >= FourthNumber)
+                {
+                    GreatestNumber = FirstNumber;
+                }
+                else
+                {
+                    GreatestNumber = FourthNumber;
+                }
+            }
+            else if (SecondNumber >= ThirdNumber && SecondNumber >= FourthNumber)
+            {
+                GreatestNumber = SecondNumber;
+            }
+            else
+            {
+                GreatestNumber = ThirdNumber;
+            }
+
+            return GreatestNumber;
+        }
+
+    }
+}
---
+++ greatest_of_4/Program.cs
@@ -0,0 +1,13 @@
+﻿using greatest_of_4;
+
+//Calling Greatest function
+
+Find_Greatest greatest = new Find_Greatest()
+{
+    FirstNumber = 0,
+    SecondNumber = 10.25M,
+    ThirdNumber = 12.34M,
+    FourthNumber = -10
+};
+
+Console.WriteLine($"The greatest number is {greatest.GreatestOf4()}");
